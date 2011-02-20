201c7243dbc39a6328449b88d23d92092e1f3e90
leandro.vazquez <leandro.vazquez@f6e52328-b042-4b89-21d3-e30d7bd090d8>
2011-02-20 07:01:08 +0000

    cambio la licencia de GPLv3 a AGPL

--- trunk/js/kanban.js
+++ trunk/js/kanban.js
@@ -1,3 +1,29 @@
+/* ***
+*
+*
+
+Copyright (C) 2010  Leandro Vázquez Cervantes (leandro[-at-]leandro[-dot-]org)
+Copyright (C) 2010  Octavio Benedí Sánchez (octaviobenedi[-at-]gmail[-dot-]com)
+Copyright (C) 2010  Verónica Pazos (verocorella[-at-]gmail[-dot-]com)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*
+*
+*
+*** */
+
 $(document).ready(function() {
   
 	/* Manipulación de columnas */
