a53aac75fc65fd8db6c4c784dcdbb6bb0dc70871
leandro.vazquez@gmail.com <leandro.vazquez@gmail.com@f6e52328-b042-4b89-21d3-e30d7bd090d8>
2012-09-18 17:01:12 +0000

    html 5 progressbar, improved layaout, portuguese translation

--- trunk/inc/functions.inc.php
+++ trunk/inc/functions.inc.php
@@ -12,6 +12,11 @@ function check_lang($lang = NULL){
       require "lng/es.lng.php";
       @setcookie("virtualkanban", 'es');
       break; 
+      
+    case 'pt':
+      require "lng/pt.lng.php";
+      @setcookie("virtualkanban", 'pt');
+      break;
   
     default:
       if ($_COOKIE["virtualkanban"]) check_lang($_COOKIE["virtualkanban"]);
